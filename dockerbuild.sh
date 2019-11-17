6b1730dfa1f987e6e0e46a5e314b622a443a6c2a
Deaconjoe <clouddjoe@gmail.com>
2019-11-17 16:35:43 +0000

    travis

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+docker image rm  --force
+docker build --no-cache .
 
-docker build -t clouddjoe1/hello_world
+docker build -t clouddjoe1/hello_world:latest .
+
 docker run -p 3333:3333 clouddjoe1/hello_world
